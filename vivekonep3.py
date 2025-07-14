0a54cec66e6ed00e1ad1b38b563cfa3cf6172b93
Jeyabharathi666 <saravananjeyabharathi@gmail.com>
2025-07-14 22:32:26 +0530

    Update vivekonep3.py

--- vivekonep3.py
+++ vivekonep3.py
@@ -18,11 +18,18 @@ URL = ["https://chartink.com/screener/copy-copy-copy-sreelakshmi-guruvayoorappan
        "https://chartink.com/screener/copy-copy-bb-blaster-2",
        "https://chartink.com/screener/copy-the-best-btst-193",
        "https://chartink.com/screener/22-nw-shesha-magic-buy-love",
-       "https://chartink.com/screener/copy-atp-above-long-cash-2"]
+       "https://chartink.com/screener/copy-atp-above-long-cash-2",
+       "https://chartink.com/screener/copy-bullish-for-next-day-future-and-option-trading-animesh-2",
+       "https://chartink.com/screener/copy-how-to-find-future-and-option-stocks-buy-entry-future",
+       "https://chartink.com/screener/22-nw-shesha-magic-buy-love-f-o",
+       "https://chartink.com/screener/agp-bullish2",
+       "https://chartink.com/screener/copy-atp-above-long-24",
+       "https://chartink.com/screener/copy-copy-future-and-option-pin-bar-pranshu-tiwari",
+       "https://chartink.com/screener/copy-copy-future-and-options-2-1-4"]
        
 sheet_id = "1QjvejkKtq0h8trJOAJCPBXGEViD5W_e52HGEvgXmBJg"
-worksheet_name = ["p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12","p13","p14","p15","p16","p17"]
-
+worksheet_name = ["p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12","p13","p14","p15","p16","p17","p18","p19","p20","p21","p22","p23","p24"]
+       
 def scrape_chartink(URL, worksheet_name):
     print(f"🚀 Starting Chartink scrape for {worksheet_name}...")
     print(f"🌐 Loading: {URL}")
